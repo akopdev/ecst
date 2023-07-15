824ac09061e7e56be3ad15bb0913b2a2cec0ba07
Akop Kesheshyann <hello@akop.devn>
2023-07-15 11:38:37 +0200

    feat: replace prefect with custom scheduler

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 
-from .settings import settings
+# from .settings import settings
 
 
-def connect() -> AsyncIOMotorCollection:
-    client = AsyncIOMotorClient(settings.DATABASE_DSL)
+def connect(dsn: str) -> AsyncIOMotorCollection:
+    try:
+        db = dsn.rsplit("/", 1)
+        client = AsyncIOMotorClient(db[0])
+    except Exception:
+        raise Exception("Wrong dsn address")
 
     if not client or not client.server_info():
         raise Exception("Failed to connect to storage.")
-    return client[settings.DATABASE_NAME]
-
-
-# store data in `events` collections
-storage = connect()
+    return client[db[1]]
--- app/flows.py
+++ app/flows.py
@@ -1,24 +1,44 @@
+from functools import wraps
+
+from motor.frameworks.asyncio import asyncio
+from typer import Option, Typer
+from typing_extensions import Annotated
+
 from . import api
 from .database import connect
 from .logger import log
 
+app = Typer(no_args_is_help=True)
 
-async def upcoming():
+
+def flow(f):
+    @app.command()
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        log.info(f"Start `{f.__name__}` job")
+        res = asyncio.run(f(*args, **kwargs))
+        log.info(f"Job `{f.__name__}` completed successfully")
+        return res
+
+    return wrapper
+
+
+@flow
+async def upcoming(
+    dsn: Annotated[str, Option()],
+):
     """Fetch upcoming events from remote server"""
-    await api.fetch(connect())
+    await api.fetch(connect(dsn))
 
-async def update():
-    """Update actual value for passed indicators"""
-    db = connect()
+
+@flow
+async def indicators(
+    dsn: Annotated[str, Option()],
+):
+    """Update actual value for passed events"""
+    db = connect(dsn)
     date_range = await api.get_date_ranges(db)
     if date_range:
         await api.fetch(db=db, **date_range)
     else:
         log.info("Nothing to update")
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    # asyncio.run(upcoming())
-    asyncio.run(update())
