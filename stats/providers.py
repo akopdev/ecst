bc8b94868be706d98fe722b653589e9049308fb1
Akop Kesheshyan <akesheshyan@actcommodities.com>
2023-08-10 17:23:35 +0200

    add custom user agent to request

--- stats/providers.py
+++ stats/providers.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 from typing import List
 
@@ -10,6 +11,12 @@ from .schemas import DataProviderResult, Event
 
 
 class DataProvider:
+    _user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+    ]
+
     async def fetch(
         self,
         date_start: datetime,
@@ -24,6 +31,7 @@ class DataProvider:
             events = []
             async with session.get(
                 "https://economic-calendar.tradingview.com/events",
+                headers={"User-Agent": random.choice(self._user_agents)},
                 params={
                     "from": date_start.isoformat() + "Z",
                     "to": date_end.isoformat() + "Z",
